2aebffec408fabf4309f7b2dcaaffbf826ec0c20
Keshav Golande <keshavgolande46@gmail.com>
2026-01-22 11:10:03 +0530

    Make Hero Section

---
+++ src/pages/Homepage/herosec.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import HeroSection from '../../components/Homepage/Herosection.jsx'
+const herosec = () => {
+  return (
+    <div>
+      <HeroSection />
+    </div>
+  )
+}
+
+export default herosec;
